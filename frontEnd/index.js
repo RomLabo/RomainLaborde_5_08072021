d774255708648d5ead7a5192cf9460e9edd5a5e6
RomL <rom.laborde@orange.fr>
2021-07-14 17:46:42 +0200

    modification index.js

--- frontEnd/index.js
+++ frontEnd/index.js
@@ -1,44 +1,31 @@
-main()
+const apiUrl = "http://localhost:3000/api/cameras";
 
-async function main() {
-    const products = await getProducts()
-    for (product of products) {
-        displayAllProduct(product)   
-    }
-}
-
-function getProducts() {
-    return fetch("http://localhost:3000/api/cameras")
-        .then(function(httpBodyResponse) {
-            return httpBodyResponse.json()
-        })
-        .then(function(products){
-            return products
-        })
-        .catch(function(error){
-            alert(error)
-        })
-}
-
-function displayAllProduct(product) {
-    const templateElement = document.getElementById("articleTemplate")
-    const cloneElement = document.importNode(templateElement.content, true)
-
-    let idProd = product._id;
-    cloneElement.getElementById("article").setAttribute("id", idProd)
-    cloneElement.getElementById("articleTitle").textContent = product.name
-    let productPrice = product.price / 1000;
-    cloneElement.getElementById("articlePrice").textContent = (new Intl.NumberFormat("fr-FR", {style: "currency", currency: "EUR"}).format(productPrice))
-    let urlImage = product.imageUrl;
-    cloneElement.getElementById("articleImage").setAttribute("src", urlImage)
-
-    document.getElementById("box").appendChild(cloneElement)
-    
-}
+fetch(apiUrl)
+    .then((response) => 
+        response.json()
+    .then((data) => {
+            //console.log(data);
+            let viewProducts = '<div class="d-flex flex-wrap">';
+            for (let product of data) {
+                viewProducts += `<a href="" class="item m-3 w-25" >
+                                    <article id="${product._id}" class="m-3">
+                                        <img src="${product.imageUrl}" alt"Un appareil photo vintage" class="w-100 rounded-top"/>
+                                        <div class="d-flex justify-content-between p-2 bg-white rounded-bottom">
+                                            <h3 class="fs-5">${product.name}</h3>
+                                            <p>${(new Intl.NumberFormat("fr-FR", {style: "currency", currency: "EUR"}).format(product.price / 1000))}</p>
+                                        </div>
+                                    </article>
+                                </a>`;
+            }
+            viewProducts += '</div>';
+            document.querySelector('#products').innerHTML = viewProducts;
+        }) 
+    ).catch((err) => console.log('Error :' + err)
+);
 
 
 
-
-
-
-
+document.querySelector('article').addEventListener('click', function(e) {
+    let target = e.target.value;
+    console.log(target);
+})
