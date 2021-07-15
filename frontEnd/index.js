91465173bf3cc029751f129ab3498ebeb8fe6e47
RomL <rom.laborde@orange.fr>
2021-07-16 01:30:35 +0200

    Ajout de style.css début desgin page d'accueil

--- frontEnd/index.js
+++ frontEnd/index.js
@@ -18,11 +18,11 @@ let getProducts = data => data.forEach(product => viewProduct(product));
 function viewProduct (product) {
     let productHtml = document.createElement('div');
         productHtml.innerHTML = 
-            `<a href="./product.html?${product._id}" class="item m-3 w-25" >
-                <article  class="m-3">
-                    <img src="${product.imageUrl}" alt"Un appareil photo vintage" class="w-100 rounded-top"/>
-                    <div class="d-flex justify-content-between p-2 bg-white rounded-bottom">
-                        <h3 class="fs-5">${product.name}</h3>
+            `<a href="./product.html?${product._id}" class="product-link" >
+                <article  class="product">
+                    <img src="${product.imageUrl}" alt"Un appareil photo vintage" class="product__img"/>
+                    <div class="product__info">
+                        <h3>${product.name}</h3>
                         <p>${(new Intl.NumberFormat("fr-FR", {style: "currency", currency: "EUR"}).format(product.price / 1000))}</p>
                     </div>
                 </article>
