d3eead8b41635066b190a27ed462b3e7f4a58955
RomL <rom.laborde@orange.fr>
2021-07-22 11:54:43 +0200

    Ajout option vider le panier sur la page panier

--- frontEnd/product.js
+++ frontEnd/product.js
@@ -20,7 +20,7 @@ fetch(apiUrl + "/" + productId)
 
 
 // Créer un article avce toutes les informations liés au produit sélectionné.
-function getProduct (data) {
+const getProduct = (data) => {
     // Ajout de ces éléments dans leur conteneur
     viewItem(data);
     // Ajout des options du produit
@@ -31,7 +31,7 @@ function getProduct (data) {
 
 
 // Ajout des valeurs du produit à chaque élément composant l'élément 'article'.
-function viewItem (data) {
+const viewItem = (data) => {
     document.getElementById("articleId").setAttribute("id", data._id);
     document.getElementById("articleTitle").textContent = data.name;
     document.getElementById("articlePrice").textContent = (new Intl.NumberFormat("fr-FR", {style: "currency", currency: "EUR"}).format(data.price / 1000));
@@ -41,38 +41,62 @@ function viewItem (data) {
 
 
 // Pour chaque objectifs du produit il sera crée une option dans l'élément 'select'
-function getLensesOption (data) {
+const getLensesOption = (data) => {
     let allLenses = data.lenses;
-    let lensesOption = `<select name="lenses" id="lenses-select">`;
+    let lensesOption = `<option value="option">Choisir une option</option>`;
         for (let lense of allLenses) {
             lensesOption += `<option value="${lense}">${lense}</option>`
         }
-    lensesOption += `</select>`;
-    document.querySelector('#option').innerHTML = lensesOption;
+    document.querySelector('#lenses-select').innerHTML = lensesOption;
 }
 
 
 // Change le texte du titre principal de la page par un message d'alerte si une érreur est détecté dans l'url.
-let urlAlert = () => {
+const urlAlert = () => {
     document.getElementById('sectionTitle').textContent = "Un problème est servenu veuillez revenir sur la page principal, Merci.";
     document.getElementById('product').style.display = 'none';
 } 
 
 
-// Ecoute le click du bouton 'ajouter au panier' et utilise la fonction 'populateStorage'.
-document.querySelector('#add').addEventListener('click', populateStorage);
+// Créer une fenêtre pour consulter son panier ou continuer ses achats
+const confirmationWindow = () => {
+    if (window.confirm(`Votre produit a bien été ajouté au panier
+    Consulter le panier OK ou continuer vos achats ANNULER`)){
+        window.location.href = "./shopping-cart.html";
+    } else {
+        window.location.href = "./index.html";
+    }
+}
 
-// Récupère les donées du produit et les envoie dans le local storage.
-function populateStorage(data) {
+
+// Récupère l'option choisie.
+let optionChoiceValue = '';
+const getOptionChoiceValue = () => {
+    let optionChoice = document.getElementById('lenses-select');
+    optionChoice.addEventListener('change', (e) => {
+        optionChoiceValue = e.target.value;
+    })
+}
+
+// Créer un objet avec les différentes valeurs du produit.
+let cartProduct = '';
+const getCartProductObject = (data) => {
+    cartProduct = {
+        image: data.imageUrl,
+        price: data.price * quantityChoiceValue,
+        nameProduct: data.name,
+        quantity: quantityChoiceValue,
+        option: optionChoiceValue,
+    }
+}
+
+// Récupère la quantity choisie et envoie les données dans le localeStorage.
+let quantityChoiceValue = '';
+const getQuantityChoiceValue = (data) => {
     let quantityOfProduct = document.getElementById('quantity');
     quantityOfProduct.addEventListener('change', (event) =>  {
-        let quantityChoiceValue = event.target.value;
-        let cartProduct = {
-            image: data.imageUrl,
-            price: data.price * quantityChoiceValue,
-            nameProduct: data.name,
-            quantity: quantityChoiceValue,
-        }
+        quantityChoiceValue = event.target.value;
+        getCartProductObject(data);
         let productStorage = JSON.parse(localStorage.getItem("viewCartProduct"))
         if (productStorage) {
             // Ajoute les données du produit dans le localStorage
@@ -85,22 +109,23 @@ function populateStorage(data) {
             productStorage.push(cartProduct);
             localStorage.setItem("viewCartProduct", JSON.stringify(productStorage));
             console.log(productStorage);
-        }
-    }); // Ajouter les options au storage
-    confirmationWindow();   
-    
+        } 
+    })
 }
 
-
-// Créer une fenêtre pour consulter son panier ou continuer ses achats
-const confirmationWindow = (data) => {
-    if (window.confirm(`${data.name} option : ${lense} a bien été ajouté au panier
-    Consulter le panier OK ou continuer vos achats ANNULER`)){
-        window.location.href = "shopping-cart.html";
-    } else {
-        window.location.href = "index.html";
-    }
+// Execute ces deux fonctions avant l'envoie dans le local storage.
+const populateStorage = (data) => {
+    getOptionChoiceValue();
+    getQuantityChoiceValue(data);
 }
+
+// Ecoute le click du bouton 'ajouter au panier' et utilise la fonction 'populateStorage'.
+document.querySelector('#add').addEventListener('click', (populateStorage, confirmationWindow));
+
+
+
+
+
 
 
 
--- frontEnd/shopping-cart.js
+++ frontEnd/shopping-cart.js
@@ -7,9 +7,40 @@ console.log(viewProductStorage);
 let viewProductStorageJSON = JSON.parse(viewProductStorage);
 console.log(viewProductStorageJSON);
 
+if (viewProductStorageJSON == null) {
+    let cartInfo = document.getElementById('cart-info');
+    cartInfo.textContent = 'Votre panier est vide';
+} 
 
 // Pour récupérer par la suite le total des prix.
 let sumProductsPriceStorage = 0;
+
+
+// Créer un titre h2 avec la somme total des produits et l'injecte dans le document.
+let lengthOfStorage = viewProductStorageJSON.length;
+let getSumPriceProductStorage = () => {
+    console.log(lengthOfStorage);
+    let sumPriceProductElement = document.createElement('h2');
+    sumPriceProductElement.innerHTML = `Total : ${(new Intl.NumberFormat("fr-FR", {style: "currency", currency: "EUR"}).format(sumProductsPriceStorage / 1000))}`;
+    console.log(sumPriceProductElement); 
+    document.getElementById('sumPriceProducts').appendChild(sumPriceProductElement);
+}
+
+
+// Créer une ligne de tableau avec les différentes valeurs du produits
+const addProductStorage = (product) => {
+    let productHtml = document.createElement('tr');
+        productHtml.setAttribute("class", "cart-product");
+        productHtml.innerHTML = 
+            `<td class="cart-product__img" ><img id="imgStor" src="${product.image}" alt="Appareil photo" /></td>
+             <td id="quantityStor" class="cart-product__quantity">x${product.quantity}</td>
+             <td class="cart-product__title"><h3 id="nameStor">${product.nameProduct}</h3></td>
+             <td id="quantityStor" class="cart-product__title">${product.option}</td>
+             <td id="priceStor" class="cart-product__price">${(new Intl.NumberFormat("fr-FR", {style: "currency", currency: "EUR"}).format(product.price / 1000))}</td>`;
+             // <td id="quantityStor" class="cart-product__quantity"><button id="removeItem" name="Remove">X</button></td>
+    // Ajout les produits dans son conteneur.
+    document.getElementById('tableStor').appendChild(productHtml);
+}
 
 
 // Pour chaque produit stocké dans le storage, est crée une ligne de tableau et son prix est ajouté au précédent.
@@ -18,29 +49,30 @@ for (product of viewProductStorageJSON) {
     sumProductsPriceStorage += product.price;
     console.log(sumProductsPriceStorage);
 }
-let lengthOfStorage = viewProductStorageJSON.length; 
 
-
-// Créer un titre h2 avec la somme total des produits et l'injecte dans le document.
-console.log(lengthOfStorage);
-let sumPriceProductElement = document.createElement('h2');
-sumPriceProductElement.innerHTML = `Total : ${(new Intl.NumberFormat("fr-FR", {style: "currency", currency: "EUR"}).format(sumProductsPriceStorage / 1000))}`;
-console.log(sumPriceProductElement); 
-document.getElementById('sumPriceProducts').appendChild(sumPriceProductElement);
-
-
-// Créer une ligne de tableau avec les différentes valeurs du produits
-function addProductStorage (product) {
-    let productHtml = document.createElement('tr');
-        productHtml.setAttribute("class", "cart-product");
-        productHtml.innerHTML = 
-            `<td class="cart-product__img" ><img id="imgStor" src="${product.image}" alt="Appareil photo" /></td>
-             <td id="quantityStor" class="cart-product__quantity">x${product.quantity}</td>
-             <td class="cart-product__title"><h3 id="nameStor">${product.nameProduct}</h3></td>
-             <td id="priceStor" class="cart-product__price">${(new Intl.NumberFormat("fr-FR", {style: "currency", currency: "EUR"}).format(product.price / 1000))}</td>`;
-    // Ajout les produits dans son conteneur.
-    document.getElementById('tableStor').appendChild(productHtml);
+const removeAll = () => {
+    localStorage.clear();
+    document.location.reload();
 }
+
+
+let removeAllProducts = document.getElementById('remove-products');
+removeAllProducts.addEventListener('click', removeAll);
+
+
+
+
+
+// Affiche le prix total.
+getSumPriceProductStorage();
+
+
+
+
+
+
+
+
 
 
 
